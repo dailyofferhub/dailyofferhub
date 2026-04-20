afd9da31f49a0365c7fb0f8852b2254dcf542b9f
dailyofferhub <tcaceace@gmail.com>
2026-04-20 19:18:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -230,6 +230,16 @@ reviews: 1437,
 description: "Upgrade your home with this charming pink flower pillow, designed to bring comfort and aesthetic beauty into your space. Made with ultra-soft fabric and a plush filling, this cute daisy cushion is perfect for your bed, sofa, or favorite relaxation spot.Its eye-catching floral design adds a cozy and stylish touch, making it ideal for modern, kawaii, or aesthetic room setups. Whether you're decorating your bedroom or looking for a unique gift, this pillow creates a warm and inviting atmosphere instantly.",
 image: "https://github.com/Aadhi6374/image/blob/main/pillow.png?raw=true",
 amazon: "https://www.amazon.com/dp/B09P9N1Z5R?_encoding=UTF8&th=1&linkCode=ll2&tag=aadhithyan637-20&linkId=d29177d286f02418ed3d612f1d9548d7&language=en_US&ref_=as_li_ss_tl"
+},
+{
+id: 24,
+name: "COOFANDY Men's 2 Pieces Cotton Linen Set Henley Shirt Long Sleeve and Casual Beach Pants Summer Yoga Outfits",
+price: 59,
+rating: 4.1,
+reviews: 3485,
+description: "Looking for a stylish and comfortable summer outfit for men? This cotton linen 2-piece set is breathable, lightweight, and perfect for beach days, vacations, and casual wear. Available in multiple colors and all sizes, it offers a relaxed fit with effortless style. Ideal for summer fashion, travel outfits, and everyday comfort. Save this pin for your wardrobe upgrade.",
+image: "https://github.com/Aadhi6374/image/blob/main/Men%20shirts.png?raw=true",
+amazon: "https://www.amazon.com/COOFANDY-Pieces-Cotton-Sleepwear-Pajamas/dp/B089Q385G5?crid=1VJ964IBUAMAS&dib=eyJ2IjoiMSJ9.bkzOLvpGixYc3JDV9GyTFhRk4KzIwnvJYXvZCcBVEsHEatOgnzYTmtGQiCC7g0ztNANStJsjJ6PbNpazCahmQNQtNMZLgmwwIX_UzsCfNA0kyDPeRXbXgMyNBu-Ua6-Ac5VA0yV0yGMSz0gg5232qowjiCnH-Jf099KQvpFn1n3EvoPPLoyJUpnUjGqPwY6Z-LwA5oqBBwPg3dY3y23On9JQjyApk9Uocno6TG1oPwl00EI8Asvp6TpZ2c7F2UF1cOZwilOHyvFhgOS5etK_STnUPDKLS3B_BlFB3Lu8sho.eFxlxwBPfw9b5BzXouXcvHRACueWbHHa7eYHZTwqCn8&dib_tag=se&keywords=COOFANDY%2BMen%27s%2B2%2BPieces%2BCotton%2BLinen%2BSet%2BHenley%2BShirt%2BLong%2BSleeve%2Band%2BCasual%2BBeach%2BPants%2BSummer%2BYoga%2BOutfits&nsdOptOutParam=true&qid=1776691935&sprefix=coofandy%2Bmen%27s%2B2%2Bpieces%2Bcotton%2Blinen%2Bset%2Bhenley%2Bshirt%2Blong%2Bsleeve%2Band%2Bcasual%2Bbeach%2Bpants%2Bsummer%2Byoga%2Boutfits%2Caps%2C566&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1&psc=1&linkCode=ll2&tag=aadhithyan637-20&linkId=6580b9837b868fb6eb6742fa6b1b155c&language=en_US&ref_=as_li_ss_tl"
 }
 ]
 
